8e5d4d5575b10d5b41edf2d4b59af406e5063bfb
mark ndungu <markkaris438@gmail.com>
2020-11-30 15:08:57 +0300

    adding username param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,6 +75,6 @@ class UsersController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def user_params
-    params.require(:user).permit(:email, :password, :role_id)
+    params.require(:user).permit(:email, :password, :username, :role_id)
   end
 end
